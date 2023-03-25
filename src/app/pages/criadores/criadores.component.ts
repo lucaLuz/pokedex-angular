ab259bbdbecf361d171c001539153993bba692d6
Luca Luz <94574250+lucaLuz@users.noreply.github.com>
2023-03-25 17:18:28 -0300

    angular.jon

--- src/app/pages/criadores/criadores.component.ts
+++ src/app/pages/criadores/criadores.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-criadores',
-  templateUrl: './Criadores.component.html',
-  styleUrls: ['./Criadores.component.scss']
+  templateUrl: './criadores.component.html',
+  styleUrls: ['./criadores.component.scss']
 })
 export class CriadoresComponent {
 
